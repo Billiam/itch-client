b36f3d928716a3b0019caf462e33b88526929b81
Billiam <billiamthesecond@gmail.com>
2023-08-28 23:27:59 -0500

    Update itch.io purchase CSV URLs

--- lib/itch_client.rb
+++ lib/itch_client.rb
@@ -33,13 +33,13 @@ module Itch
     EDIT_GAME = "https://itch.io/game/edit/%<id>d"
     GAME = "https://%<username>s.itch.io/%<slug>s"
     LOGIN = "https://itch.io/login"
-    MONTH_PURCHASES_CSV = "https://itch.io/export-purchases/by-date/%<month>d-%<year>d"
-    PURCHASES_CSV = "https://itch.io/export-purchases/all"
+    MONTH_PURCHASES_CSV = "https://itch.io/dashboard/export-purchases/by-date/%<month>d-%<year>d"
+    PURCHASES_CSV = "https://itch.io/dashboard/export-purchases/all"
     REVIEWS = "https://itch.io/game/ratings/%<id>d?page=%<page>d"
     REWARD_CSV = "https://itch.io/game/rewards/%<id>d/claimed?format=csv"
     REWARDS = "https://itch.io/game/rewards/%<id>d"
     TOTP_FRAGMENT = "https://itch.io/totp/verify/"
-    YEAR_PURCHASES_CSV = "https://itch.io/export-purchases/by-date/%<year>d"
+    YEAR_PURCHASES_CSV = "https://itch.io/dashboard/export-purchases/by-date/%<year>d"
   end
 
   def self.new(**kwargs)
